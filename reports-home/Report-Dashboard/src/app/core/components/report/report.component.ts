1cbaeaac0b62726f56405335cdfc9a4117ece932
Msrivastava_ic <Msrivastava_ic@fishbowl.com>
2019-12-11 17:54:40 +0530

    removing the header from dsashboard

--- reports-home/Report-Dashboard/src/app/core/components/report/report.component.ts
+++ reports-home/Report-Dashboard/src/app/core/components/report/report.component.ts
@@ -22,7 +22,7 @@ export class ReportComponent implements OnInit {
       var frameDiv = document.getElementById('fm');
       if(this.report.dashboardId){ 
         var ifrm = document.createElement("iframe");
-        ifrm.setAttribute("src"," http://10.200.10.21:8081/app/main#/dashboards/"+this.report.dashboardId+"?embed=true?filter="+ brandFilter);
+        ifrm.setAttribute("src"," http://10.200.10.21:8081/app/main#/dashboards/"+this.report.dashboardId+"?embed=true&h=false&l=false&t=false?filter="+ brandFilter);
         ifrm.style.width = "100%";
         ifrm.style.height = "100%";
         ifrm.style.paddingLeft = "4%";
