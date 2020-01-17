199010fe057da3f552df29504361e7c9483052d9
Msrivastava_ic <Msrivastava_ic@fishbowl.com>
2020-01-17 19:32:06 +0530

    on app exit removing cookies

--- reports-home/Report-Dashboard/src/app/app.component.ts
+++ reports-home/Report-Dashboard/src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { CookieService } from 'ngx-cookie-service';
-import {AuthService} from '../app/shared/services/auth-service';
 
 @Component({
   selector: 'app-root',
@@ -9,22 +7,4 @@ import {AuthService} from '../app/shared/services/auth-service';
 })
 export class AppComponent {
   title = 'Report-dashboard';
-  constructor(private _authService:AuthService , private cookieService :CookieService) {
-   
-  }
-  OnAppExit(){
-    if(location.href.indexOf('reportDashboard')== -1 || location.href.indexOf('reportList')==-1){
-        // to delete the cookies 
-      if(this.cookieService.get('_irecube')){
-        this.cookieService.set("_irecube", '', new Date("Thu, 01 Jan 1970 00:00:01 GMT"), '/' , 'ir2qa.fishbowl.com');
-      }
-      if(this._authService.isSisenseCookieExist()){
-        this.cookieService.set("_irsession_id", '', new Date("Thu, 01 Jan 1970 00:00:01 GMT"), '/' , '.ir2qa.fishbowl.com');
-      }
-      if(this.cookieService.check('.prism_shared')){
-        this.cookieService.set(".prism_shared", '', new Date("Thu, 01 Jan 1970 00:00:01 GMT"), '/' , 'sisense.fishbowl.com');
-      }
-    }
-    
-  }
 }
