83765e4bddf1b97c14d3d021ffbe0e5dc89e9883
Msrivastava_ic <Msrivastava_ic@fishbowl.com>
2020-02-13 12:00:18 +0530

    adding new dashboard 'Mailing Comparison'

--- reports-home/Report-Dashboard/src/app/core/models/mock-reports.ts
+++ reports-home/Report-Dashboard/src/app/core/models/mock-reports.ts
@@ -2,5 +2,6 @@ import { Report } from './reports';
 
 export const REPORTLIST: Report[] = [
   { id: 1, name: 'Mailing Summary' , dashboardId: '5e356face2d38411e4a4869f' },
-  { id: 2, name: 'Member Summary' , dashboardId: '5d79330f89f99c1e10a086b7' }
+  { id: 2, name: 'Member Summary' , dashboardId: '5d79330f89f99c1e10a086b7' },
+  { id: 2, name: 'Mailing Comparison' , dashboardId: '5e42b920e2d38411e4a48f3d' }
 ];
