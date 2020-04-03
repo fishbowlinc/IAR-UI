b99d2b90400d8031717d0439d13dd294311334ec
Msrivastava_ic <Msrivastava_ic@fishbowl.com>
2020-04-03 20:21:56 +0530

    changing the dashboard ID for member summary

--- reports-home/Report-Dashboard/src/app/core/models/mock-reports.ts
+++ reports-home/Report-Dashboard/src/app/core/models/mock-reports.ts
@@ -4,5 +4,4 @@ export const REPORTLIST: Report[] = [
   { id: 1, name: 'Mailing Summary', dashboardId: '5e356face2d38411e4a4869f' },
   { id: 2, name: 'Mailing Comparison', dashboardId: '5e42b920e2d38411e4a48f3d' },
   { id: 2, name: 'Member Summary', dashboardId: '5e6b9143d47fdc0ef0aa5b98' }
-
 ];
